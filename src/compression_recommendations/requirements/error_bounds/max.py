688936530c1094eaec93517eee3eba5e0d2a9d1c
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-09-01 11:22:42 +0300

    Document more requirements

--- src/compression_recommendations/requirements/error_bounds/max.py
+++ src/compression_recommendations/requirements/error_bounds/max.py
@@ -23,6 +23,25 @@ __all__ = [
 
 @dataclass(kw_only=True, slots=True)
 class MaxPointwiseAbsoluteErrorBoundRequirement(Requirement):
+    r"""
+    Require that the maximum pointwise absolute error is bounded.
+
+    \[
+    R_{\text{\tiny max-pointwise-absolute-error-bound}(\epsilon_{\text{abs}})}(x_i, \hat{x}_i) := \begin{cases}
+        \hat{x}_i \equiv \text{NaN} \quad &\text{if } x_i \equiv \text{NaN} \\
+        \hat{x}_i = x_i \quad &\text{if } x_i \in \{ -\infty, \infty \} \\
+        |\hat{x}_i - x_i| \leq \epsilon_{\text{abs}} \quad &\text{otherwise}
+    \end{cases}
+    \]
+
+    The absolute error bound $\epsilon_{\text{abs}}$ must be non-negative and finite.
+
+    Requirements $R$ are defined for each data point $x_i$ and its decompressed
+    reconstruction $\hat{x}_i$, i.e. $R(x_i, \hat{x}_i)$.
+    See the [`AnyRequirement`][....combinators.AnyRequirement] for more
+    information.
+    """
+
     kind: ClassVar[RequirementKind] = RequirementKind.max_pointwise_absolute_error_bound
     value: int | float
 
@@ -49,6 +68,25 @@ class MaxPointwiseAbsoluteErrorBoundRequirement(Requirement):
 
 @dataclass(kw_only=True, slots=True)
 class MaxPointwiseRelativeErrorBoundRequirement(Requirement):
+    r"""
+    Require that the maximum pointwise relative error is bounded.
+
+    \[
+    R_{\text{\tiny max-pointwise-relative-error-bound}(\epsilon_{\text{rel}})}(x_i, \hat{x}_i) := \begin{cases}
+        \hat{x}_i \equiv \text{NaN} \quad &\text{if } x_i \equiv \text{NaN} \\
+        \hat{x}_i = x_i \quad &\text{if } x_i \in \{ -\infty, \infty \} \\
+        |\hat{x}_i - x_i| \leq |x_i| \cdot \epsilon_{\text{rel}} \quad &\text{otherwise}
+    \end{cases}
+    \]
+
+    The relative error bound $\epsilon_{\text{rel}}$ must be non-negative and finite.
+
+    Requirements $R$ are defined for each data point $x_i$ and its decompressed
+    reconstruction $\hat{x}_i$, i.e. $R(x_i, \hat{x}_i)$.
+    See the [`AnyRequirement`][....combinators.AnyRequirement] for more
+    information.
+    """
+
     kind: ClassVar[RequirementKind] = RequirementKind.max_pointwise_relative_error_bound
     value: int | float
 
@@ -75,6 +113,36 @@ class MaxPointwiseRelativeErrorBoundRequirement(Requirement):
 
 @dataclass(kw_only=True, slots=True)
 class MaxPointwiseRangeRelativeErrorBoundRequirement(Requirement):
+    r"""
+    Require that the maximum pointwise range-relative error is bounded.
+
+    \[
+    R_{\text{\tiny max-pointwise-range-relative-error-bound}(\epsilon_{\text{range-rel}})}(x_i, \hat{x}_i) := \begin{cases}
+        \hat{x}_i \equiv \text{NaN} \quad &\text{if } x_i \equiv \text{NaN} \\
+        \hat{x}_i = x_i \quad &\text{if } x_i \in \{ -\infty, \infty \} \\
+        |\hat{x}_i - x_i| \leq x_{\text{range}} \cdot \epsilon_{\text{range-rel}} \quad &\text{otherwise}
+    \end{cases}
+    \]
+
+    where
+
+    \[
+    \begin{align*}
+        x_{\text{range}} &:= x_{\text{finite-max}} - x_{\text{finite-min}} \\
+        x_{\text{finite-max}} &:= \max_i \{ x_i \mathbin{|} x_i \not \equiv \text{NaN} \land x_i \not \in \{ -\infty, \infty \} \} \\
+        x_{\text{finite-min}} &:= \min_i \{ x_i \mathbin{|} x_i \not \equiv \text{NaN} \land x_i \not \in \{ -\infty, \infty \} \}
+    \end{align*}
+    \]
+
+    The range-relative error bound $\epsilon_{\text{range-rel}}$ must be
+    non-negative and finite.
+
+    Requirements $R$ are defined for each data point $x_i$ and its decompressed
+    reconstruction $\hat{x}_i$, i.e. $R(x_i, \hat{x}_i)$.
+    See the [`AnyRequirement`][....combinators.AnyRequirement] for more
+    information.
+    """
+
     kind: ClassVar[RequirementKind] = (
         RequirementKind.max_pointwise_range_relative_error_bound
     )
